76eb1e848f07b17021e14d6566a416236e2e0d00
Sweetyamnesia <angela.r@live.fr>
2025-04-14 14:27:17 +0200

    ajout du fichier prompt.c

---
+++ prompt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prompt.c
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char *buffer;
+	size_t bufsize = 32;
+	size_t characters;
+
+	buffer = (char *)malloc(bufsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Unable to allocate buffer");
+		exit(1);
+	}
+
+	printf("$ ");
+	characters = getline(&buffer, &bufsize, stdin);
+	printf("%zu characters were read.\n", characters);
+	printf("You typed: '%s'\n", buffer);
+
+	return (0);
+}
