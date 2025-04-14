3784765f896f3801e18336c272eab7b13cd731bc
Sweetyamnesia <angela.r@live.fr>
2025-04-14 15:56:28 +0200

    ajout du fichier pour l'utilisation de strtok

---
+++ 2.av.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - 2.av.c
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char str[] = "Geeks-for-Geeks";
+	char *token;
+
+	token = strtok(str, "-");
+
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " - ");
+	}
+
+	return (0);
+}
