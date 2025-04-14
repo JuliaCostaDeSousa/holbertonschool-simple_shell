6738fd7fd2543928afcc3d7851f804b1e55f3cbe
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-14 15:00:47 +0200

    creation et ecriture du fichier prompt.c

---
+++ start/prompt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc: nb of argument
+ * @argv: value og argument
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+    char *buffer = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    printf("$ ");
+    read = getline(&buffer, &len, stdin);
+    if (read != -1)
+    printf("%s", buffer);
+    free(buffer);
+    return (0);
+}
