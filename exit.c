b2b6014f97dd440563817e94664b606df2e207e9
Sweetyamnesia <angela.r@live.fr>
2025-04-22 17:01:50 +0200

    ajout de la fonction handle_exit

---
+++ exit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * handle_exit - termine proprement le processus en libérant la mémoire
+ * @buffer: pointeur vers la ligne de commande à libérer
+*/
+
+void handle_exit(char *buffer)
+{
+	free(buffer);
+	exit(0);
+}
+
