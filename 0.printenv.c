643d07a02a7228802be28adf8c85cffa55607eec
Sweetyamnesia <angela.r@live.fr>
2025-04-22 15:42:10 +0200

    modification : changement du main en une fontion qui imprime l'environnement

--- 0.printenv.c
+++ 0.printenv.c
@@ -1,24 +1,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - main
-* @ac: the number of arguments passed to the program
- * @av: an array of strings (pointers to char)
- * @env: array of strings that represents the environment
+ * print_env - main
  * Return: always 0
  */
 
-int main(int ac, char **av, char **env)
+void print_env(void)
 {
 	int i = 0;
-	extern char **environ;
 
 	while (environ[i])
 	{
 		printf("%s\n", environ[i++]);
 	}
-
-	return (0);
 }
