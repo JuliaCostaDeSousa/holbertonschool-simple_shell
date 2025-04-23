52da1847879fb14546e9afa99430e864e361dff8
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-23 14:18:01 +0200

    modification fonction exit

--- exit.c
+++ exit.c
@@ -1,13 +1,22 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * handle_exit - termine proprement le processus en libérant la mémoire
- * @buffer: pointeur vers la ligne de commande à libérer
+ * handle_exit - quitte le shell proprement
+ * @buffer: ligne de commande à libérer
+ * @words: tableau de mots à libérer
 */
 
-void handle_exit(char *buffer)
+void handle_exit(char *buffer, char **words)
 {
-	free(buffer);
+	if (buffer)
+		free(buffer);
+	if (words)
+		free_array(words);
+
 	exit(0);
 }
+
 
