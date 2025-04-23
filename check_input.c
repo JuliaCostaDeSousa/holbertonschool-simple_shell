fec039cd9326d90d6fe41aabb790e87f954733a9
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-23 15:09:10 +0200

    prise en charge des espaces vides

--- check_input.c
+++ check_input.c
@@ -28,7 +28,7 @@ int check_input(char *buffer)
 	{
 		handle_exit(buffer);
 	}
-	if (buffer[0] == '\n' || buffer[0] == '\0')
+	if (strlen(buffer) == 0 || buffer[0] == '\n' || buffer[0] == '\0' || buffer[0] == ' ')
 	return (1);
 
 	return (0);
