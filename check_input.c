47414dbbe9f038a151908c5494e5e21de81dd233
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-04-23 14:32:42 +0200

    modification de la comparaison de exit en supprimant newline a la fin

--- check_input.c
+++ check_input.c
@@ -8,12 +8,12 @@
 
 int check_input(char *buffer)
 {
-	if (strcmp(buffer, "env\n") == 0)
+	if (strcmp(buffer, "env") == 0)
 	{
 		print_env();
 		return (1);
 	}
-	else if (strcmp(buffer, "exit\n") == 0)
+	else if (strcmp(buffer, "exit") == 0)
 	{
 		handle_exit(buffer);
 	}
